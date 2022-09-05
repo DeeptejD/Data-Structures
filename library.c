1d2605131968b0c78148b5eef31db4ae435d0701
DeeptejD <deeptejdhauskar2003@gmail.com>
2022-09-05 17:34:00 +0530

    new file:   .gitignore 	new file:   library.c

---
+++ library.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(int argc, char const *argv[])
+{
+    printf("===========\nABC Library\n===========\n");
+    while(1){
+        printf("Enter the number corresponding to the choice of operation\n");
+        int choice;
+        scanf("%d", &choice);
+    }
+    return 0;
+}
