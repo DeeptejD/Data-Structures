2d1cc90b7bf13ed683dca4df41bcc19666e94a5d
DeeptejD <deeptejdhauskar2003@gmail.com>
2022-09-23 21:54:31 +0530

    modified:   Linked Lists/polynomial_multiply.c 	modified:   Linked Lists/polynomial_sum.c

--- Linked Lists/polynomial_sum.c
+++ Linked Lists/polynomial_sum.c
@@ -9,20 +9,49 @@ struct node
 struct node *create(struct node *);
 struct node *add(struct node *, struct node *);
 void display(struct node *);
+struct node *multiply(struct node *, struct node *);
+
 int main()
 {
 	struct node *p1 = NULL, *p2 = NULL, *p3 = NULL;
-	printf("Enter the first polynomial\n");
-	p1 = create(p1);
-	printf("Enter the second polynomial\n");
-	p2 = create(p2);
-	p3 = add(p1, p2);
-	printf("The first polynomial is: ");
-	display(p1);
-	printf("The second polynomial is: ");
-	display(p2);
-	printf("The sum of the two polynomials is: ");
-	display(p3);
+	int choice;
+	printf("Polynomial Arithmetic\n1.Add\n2.Multiply\n3.Modify\n4.Exit\n");
+	scanf("%d", &choice);
+	switch (choice)
+	{
+	case 1:
+		printf("Enter the first polynomial\n");
+		p1 = create(p1);
+		printf("Enter the second polynomial\n");
+		p2 = create(p2);
+		p3 = add(p1, p2);
+		printf("The first polynomial is: ");
+		display(p1);
+		printf("The second polynomial is: ");
+		display(p2);
+		printf("The sum of the two polynomials is: ");
+		display(p3);
+		break;
+	case 2:
+		printf("Enter the first polynomial\n");
+		p1 = create(p1);
+		printf("Enter the second polynomial\n");
+		p2 = create(p2);
+		printf("The first polynomial is: ");
+		display(p1);
+		printf("The second polynomial is: ");
+		display(p2);
+		p3 = multiply(p1, p2);
+		printf("The product of the two polynomials is: ");
+		display(p3);
+		break;
+	case 3:
+		break;
+	case 4:
+		exit(1);
+	default:
+		break;
+	}
 	return 0;
 }
 struct node *create(struct node *start)
@@ -163,4 +192,36 @@ void display(struct node *start)
 			printf(" + ");
 	}
 	printf("\n");
+}
+
+struct node *multiply(struct node *start1, struct node *start2)
+{
+    struct node *start3, *tmp, *ptr1 = start1, *ptr2 = start2;
+    start3 = NULL;
+	if(ptr==NULL || ptr2==NULL){
+		printf("The product will be 0\n");
+		exit(1);
+	}
+    while (ptr1 != NULL)
+    {
+        while (ptr2 != NULL)
+        {
+            tmp = (struct node *)malloc(sizeof(struct node));
+            tmp->coeff = ptr1->coeff * ptr2->coeff;
+            tmp->exp = ptr1->exp + ptr2->exp;
+            tmp->next = NULL;
+            if (start3 == NULL)
+                start3 = tmp;
+            else
+            {
+                struct node *ptr3 = start3;
+                while (ptr3->next != NULL)
+                    ptr3 = ptr3->next;
+                ptr3->next = tmp;
+            }
+			ptr2 = ptr2->next;
+        }
+		ptr1 = ptr1->next;
+    }
+    return start3;
 }
