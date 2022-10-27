8869944043210f80348b663132c2e8c5a2580721
DeeptejD <deeptejdhauskar2003@gmail.com>
2022-10-27 17:05:22 +0530

    Changes to be committed: 	new file:   Trees/Binary Trees/Linked_Representation.c Untracked files: 	Miscellaneous/

---
+++ Trees/Binary Trees/Linked_Representation.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+struct node
+{
+    struct node *LC; // Left Child
+    int info;
+    struct node *RC; // Right Child
+} *root = NULL;
+
+int main(int argc, char const *argv[])
+{
+
+    return 0;
+}
