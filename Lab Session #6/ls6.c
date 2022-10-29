243920884d14b41fd984c1f34f1ba7dc1ff3a2b1
DeeptejD <deeptejdhauskar2003@gmail.com>
2022-10-29 10:38:38 +0530

    Changes to be committed: 	new file:   Lab Session #6/ls6.c
    Changes not staged for commit:
    	modified:   Trees/Binary Trees/traversal.c
    
    Untracked files:
    	Lab Session #6/ls6_1.c

---
+++ Lab Session #6/ls6.c
@@ -0,0 +1,193 @@
+#include <stdio.h>
+#include <stdlib.h>
+struct node
+{
+    struct node *prev;
+    int info;
+    struct node *next;
+};
+
+struct node *addatbeg(struct node *start, int n)
+{
+    struct node *tmp;
+    if ((tmp = (struct node *)malloc(sizeof(struct node))) == NULL)
+    {
+        printf("NO mem\n");
+        exit(1);
+    }
+    tmp->info = n;
+    tmp->next = start;
+    tmp->prev = NULL;
+    start = tmp;
+    return start;
+}
+
+struct node *addatend(struct node *start, int n)
+{
+    struct node *ptr = start, *tmp;
+    if (start == NULL)
+        addatbeg(start, n);
+    while (ptr->next != NULL)
+        ptr = ptr->next;
+    if ((tmp = (struct node *)malloc(sizeof(struct node))) == NULL)
+    {
+        printf("no mem\n");
+        exit(1);
+    }
+    tmp->info = n;
+    tmp->next = ptr->next;
+    tmp->prev = ptr;
+    ptr->next = tmp;
+    return start;
+}
+
+struct node *toDigits(int n, struct node *start)
+{
+    start = NULL;
+    start = addatbeg(start, n % 10);
+    n /= 10;
+    while (n > 0)
+    {
+        start = addatend(start, n % 10);
+        n /= 10;
+    }
+    return start;
+}
+
+void display(struct node *start)
+{
+    if (start == NULL)
+    {
+        printf("The list is empty\n");
+        return;
+    }
+    struct node *ptr = start;
+    while (ptr != NULL)
+    {
+        printf("%3d", ptr->info);
+        ptr = ptr->next;
+    }
+    printf("\n");
+}
+
+int numnodes(struct node *start)
+{
+    int count = 0;
+    struct node *ptr = start;
+    while (ptr != NULL)
+    {
+        count++;
+        ptr = ptr->next;
+    }
+    return count;
+}
+
+// struct node *add(struct node *num1, struct node *num2, struct node *result)
+// {
+//     struct node *ptr1, *ptr2;
+//     if (numnodes(num1) >= numnodes(num2))
+//     {
+//         ptr1 = num1;
+//         ptr2 = num2;
+//     }
+//     else
+//     {
+//         ptr1 = num2;
+//         ptr2 = num1;
+//     }
+
+//     while (ptr1 != NULL || ptr2 != NULL)
+//     {
+//         int sum = ptr1->info + ptr2->info;
+//         if (sum > 9)
+//         {
+//             result = addatbeg(result, sum % 10);
+//             sum/=10;
+//             ptr1->next->info += sum;
+//             ptr1 = ptr1->next;
+//             ptr2 = ptr2->next;
+//         }
+//         else
+//         {
+//             result = addatbeg(result, sum);
+//             ptr1 = ptr1->next;
+//             ptr2 = ptr2->next;
+//         }
+//     }
+//     while (ptr1 != NULL)
+//     {
+//         addatbeg(result, ptr1->info);
+//         ptr1 = ptr1->next;
+//     }
+//     while (ptr2 != NULL)
+//     {
+//         addatbeg(result, ptr2->info);
+//         ptr2 = ptr2->next;
+//     }
+//     return result;
+// }
+
+struct node *add(struct node *num1, struct node *num2, struct node *result)
+{
+    // selecting the smaller list
+    struct node *ptr1, *ptr2;
+    if (numnodes(num1) > numnodes(num2))
+        ptr1 = num1, ptr2 = num2;
+    else if (numnodes(num2) > numnodes(num1))
+        ptr1 = num2, ptr2 = num1;
+    else
+        ptr1 = num1, ptr2 = num2;
+    int sum = 0;
+    // making them the same size by adding zeroes
+    while (numnodes(ptr1) != numnodes(ptr2))
+        addatend(ptr2, 0);
+
+    // finding sum for the first time
+    sum = ptr1->info + ptr2->info;
+    if (sum > 9)
+    {
+        result = addatbeg(result, sum % 10);
+        sum /= 10;
+        if (ptr1->next != NULL)
+            ptr1->next->info += sum * 10;
+        else
+        {
+            result = addatend(result, sum);
+            return;
+        }
+    }
+    else
+    {
+        result = addatbeg(result, sum);
+    }
+    ptr1 = ptr1->next;
+    ptr2 = ptr2->next;
+    while (ptr1 != NULL)
+    {
+        sum = ptr1->info + ptr2->info;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    struct node *start = NULL; // start pointer
+    printf("Enter a number: ");
+    int num;
+    scanf("%d", &num);
+    start = toDigits(num, start);
+    display(start);
+    struct node *num1, *num2; // number pointers
+    int n1, n2;
+    printf("Enter first number: ");
+    scanf("%d", &n1);
+    printf("Enter second number: ");
+    scanf("%d", &n2);
+    num1 = toDigits(n1, num1);
+    num2 = toDigits(n2, num2);
+
+    struct node *results = NULL;
+    results = add(num1, num2, results);
+    printf("The sum of the two numbers is: ");
+    display(results);
+    return 0;
+}
